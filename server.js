7c1304474c7aaa6c1700925979d8b546f69fcccf
Saravanan KR <krsacme@gmail.com>
2015-10-21 13:10:01 +0530

    Added logs

--- server.js
+++ server.js
@@ -11,12 +11,20 @@ app.engine('html', require('ejs').renderFile);
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+console.log('process.env.OPENSHIFT_MONGODB_DB_URL = ' + process.env.OPENSHIFT_MONGODB_DB_URL)
+console.log('process.env.MONGO_URL = ' + process.env.MONGO_URL)
+console.log('process.env.DATABASE_SERVICE_NAME = ' + process.env.DATABASE_SERVICE_NAME)
 var mongoURLLabel = "";
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
   var mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
   var mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
   var mongoUser = process.env.MONGODB_USER
+  console.log('mongoHost = ' + mongoHost)
+  console.log('mongoPort = ' + mongoPort)
+  console.log('mongoUser = ' + mongoUser)
+  console.log('pprocess.env.MONGODB_PASSWORD = ' + process.env.MONGODB_PASSWORD)
+  console.log('mongoUser = ' + mongoUser)
   if (mongoHost && mongoPort && process.env.MONGODB_DATABASE) {
     mongoURLLabel = mongoURL = 'mongodb://';
     if (process.env.MONGODB_USER && process.env.MONGODB_PASSWORD) {
@@ -32,6 +40,7 @@ var db = null;
 var dbDetails = new Object();
 
 var initDb = function(callback) {
+  console.log('initDb: mongoURL = ' + mongoURL)
   if (mongoURL == null) return;
 
   var mongodb = require('mongodb');  
